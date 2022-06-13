4b2b031835cdd69d846c438bfed0f8cd81011ddb
Marcin Piwonski <marcin.piwonski98@gmail.com>
2022-06-13 14:15:16 +0200

    Changes suggested by Asia

--- auth.js
+++ auth.js
@@ -2,18 +2,16 @@ lpTag.identities.push(identityFn);
 
 function identityFn(callback) {
   callback({
-    sub: "+972-3-5555-555",
     iss: "https://jwt.io",
-    aud: "acc:qa57221676",
-    exp: 1534971930,
+    exp: 2034971930,
     iat: 1471899942,
-    given_name: "Eitan",
     acr: "loa1",
+    sub: "1234567890",
   });
 }
 
 const token =
-  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIrOTcyLTMtNTU1NS01NTUiLCJpc3MiOiJodHRwczovL2p3dC5pbyIsImF1ZCI6ImFjYzpxYTU3MjIxNjc2IiwiZXhwIjoxNTM0OTcxOTMwLCJpYXQiOjE0NzE4OTk5NDIsImdpdmVuX25hbWUiOiJFaXRhbiIsImFjciI6ImxvYTEifQ.NI9_je6Bm2kC75Qx8Bt1jHQh1x_w1lzgddI2pMynKG8lhEZOykIlmRFnc75yVKlaTD9NIY_MCZBDgvtcDzq0N7phYmFug08mwCHMIlYvYt1KkhZv1Zimx0IyhDpPytxix1S4GxmVtUnb7SX2bpDhKa_oKS_6rNcMBH0bEuwnSHowVcxXoUYbdgqeOA66DRJqT45bwgo332xCNmUBYG4DONSe5opKhsb-7sKMFVJ4H4WZ7Zdd5bEjcr5UKC6nxoy0rLuup_JVH9DgAzs30uBkf56m89-8ABp9frNcOj15bZSD0n4zyPrdQAL-KHq_Zew96uVhEx-PLxeoUjAYuD67zA";
+  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaXNzIjoiaHR0cHM6Ly9qd3QuaW8iLCJleHAiOjIwMzQ5NzE5MzAsImlhdCI6MTQ3MTg5OTk0MiwiYWNyIjoibG9hMSJ9.I0vVc-xmSYuEdfiv29CjsXKACLZuNvQutrC9o6YyI6zHNU2VBPlWYvjE44YC92iUwReF5NVu-etnhRCdHVWsBFNRLtTxLOM64zP-RZB3jBvFAZJQOh4-9X3AKTHUrc6qg6E6SYhllOkpwy4ZNunbb8HSi0gbL2utzbzNkPGqUrzo-0LdqTstEVWg7_MWhbV3FIcpXLoOEOfGSUkEuBxHXzc4NCCpOhiXamG29DikOzImCczxjDGIbIWfGV0_RsqmPYFctdj_s1NzUo6CjlhHPqTQsW0ZxlRWPJ6ixpUCkv6AQpcD2A3OQmJ3AqFOasCKyBagXAurYjRvF2sP_jG2HA";
 
 function lpGetAuthenticationToken(callback) {
   console.log("Sending a token...");
