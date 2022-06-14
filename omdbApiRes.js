6663232efb95a810716f0f5feee8b97fcf471cef
Marcin Piwonski <marcin.piwonski98@gmail.com>
2022-06-14 17:49:50 +0200

    UI changes in OMDbWidget.html

--- omdbApiRes.js
+++ omdbApiRes.js
@@ -1,7 +1,8 @@
 lpTag.agentSDK.init({});
 
 const inputUserMessage = document.getElementById("userMessage");
-const apiResponse = document.getElementById("apiResponse");
+const movieTitleDiv = document.getElementById("movieTitle");
+const movieYearDiv = document.getElementById("movieYear");
 
 const updateCallback = function (data) {
   const newLine = data.newValue;
@@ -30,8 +31,10 @@ const getResponse = async function () {
     const url = `https://www.omdbapi.com/?apikey=${apiKey}&t=${movieTitle}`;
     const response = await fetch(url);
     const responseJson = await response.json();
+    console.log(responseJson);
 
-    apiResponse.innerHTML = responseJson;
+    movieTitleDiv.innerHTML = `Movie title: ${responseJson.Title}`;
+    movieYearDiv.innerHTML = `Movie year: ${responseJson.Year}`;
   } catch (err) {
     console.log(err);
   }
