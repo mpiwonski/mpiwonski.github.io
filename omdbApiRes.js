9a66498390c9db001376e3e12c12d054c79c68ae
Marcin Piwonski <marcin.piwonski98@gmail.com>
2022-06-14 17:15:05 +0200

    omdbWidget.html and omdbApiRes.js modified

--- omdbApiRes.js
+++ omdbApiRes.js
@@ -1,13 +1,14 @@
 lpTag.agentSDK.init({});
 
-const divUserMessage = document.getElementById("userMessage");
+const inputUserMessage = document.getElementById("userMessage");
+const apiResponse = document.getElementById("apiResponse");
 
 const updateCallback = function (data) {
   const newLine = data.newValue;
   console.log(newLine);
 
   if (newLine[0].by === "Visitor") {
-    divUserMessage.innerHTML = newLine[0].text;
+    inputUserMessage.innerHTML = newLine[0].text;
     console.log(newLine[0].text);
   }
 };
@@ -21,24 +22,22 @@ const pathToData = "chatTranscript.lines";
 
 lpTag.agentSDK.bind(pathToData, updateCallback, notifyWhenDone);
 
-// const omdb = document.getElementById("omdb");
-// const userMessage = document.getElementById("userMessage");
-// const apiKey = `e4ce0003`;
+const apiKey = `e4ce0003`;
 
-// async function getResponse() {
-//   try {
-//     const movieTitle = userMessage.value;
-//     const url = `http://www.omdbapi.com/?apikey=${apiKey}&t=${movieTitle}`;
-//     const response = await fetch(url);
-//     const responseJson = await response.json();
+const getResponse = async function () {
+  try {
+    const movieTitle = userMessage.value;
+    const url = `http://www.omdbapi.com/?apikey=${apiKey}&t=${movieTitle}`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
 
-//     return responseJson;
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
+    apiResponse.innerHTML = responseJson;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-// omdb.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   getResponse();
-// });
+omdb.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getResponse();
+});
